175932bcde135689997aed73b6878d0b01f3cfef
pasjtene <pasjtene@yahoo.co.uk>
2022-08-10 12:23:59 +0100

    host location

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -20,15 +20,32 @@ const instance = axios.create({
 
 
 //const USERS_REST_API_URL = 'http://localhost:8080/talodu/api/users';
+
+const mylocation = window.location.origin;
 const USERS_REST_API_URL = 'http://51.68.196.188:8080/talodu/api/users';
+const LOCAL_USERS_REST_API_URL = 'http://localhost:8086/api/users';
 
 const DELETE_USERS_REST_API_URL = "http://localhost:8085/api/deleteusers";
 
 class UserService {
     //axios.defaults.withCredentials = true
 
+    
+
     getUsers() {
-       return axios.get(USERS_REST_API_URL);
+        console.log("The current location...", window.location.origin);
+        if(mylocation === "http://localhost:3000") {
+            console.log("Yes, we are local");
+            return axios.get(LOCAL_USERS_REST_API_URL);
+            //return axios.get(mylocation+":8086/api/users");
+
+        } else {
+            console.log("We are on the server, we are local");
+            return axios.get(USERS_REST_API_URL);
+
+        }
+    
+       //return axios.get(USERS_REST_API_URL);
     }
 
     deleteUsersCSV(data) {
